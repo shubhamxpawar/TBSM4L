c4754d2aefd51a133970268ee1e876972e2630e8
Shubham <realitymaniac7@gmail.com>
2025-06-06 03:37:13 +0530

    album bg

--- src/pages/N.jsx
+++ src/pages/N.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Music_card } from '../components/Music_card'
 
 export const N = () => {
   return (
-    <div>N</div>
+    <div className='pb-0 flex justify-center absolute w-full h-full max-h-auto bg-[url(https://i.ytimg.com/vi/x06EOpzffuI/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBBIQvhGqNgDN8t-KSZbIsLVtw4Wg)] bg-cover bg-center bg-no-repeat '>
+      <div className='mx-6 mt-26 p-3 w-full h-full bg-slate-800/50 rounded-3xl backdrop-blur-2xs'> 
+        <Music_card no="1" songname="Intro" artist="Seedhe Maut" len="0:25" className=""/>
+      </div>
+    </div>
   )
 }
--- src/pages/Nayaab.jsx
+++ src/pages/Nayaab.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Music_card } from '../components/Music_card'
 
 export const Nayaab = () => {
   return (
-    <div>Nayaab</div>
+    <div className='pb-0 flex justify-center absolute w-full h-full max-h-auto bg-[url(https://i.imgur.com/I4eY9s0.jpeg)] bg-cover bg-center bg-no-repeat '>
+      <div className='mx-6 mt-26 p-3 w-full h-full bg-slate-800/50 rounded-3xl backdrop-blur-2xs'> 
+        <Music_card no="1" songname="Intro" artist="Seedhe Maut" len="0:25" className=""/>
+      </div>
+    </div>
   )
 }
