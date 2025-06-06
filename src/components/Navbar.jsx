84f95c4202645f061fc7e3dc7fd16c85e163e299
Shubham <realitymaniac7@gmail.com>
2025-06-06 17:17:45 +0530

    responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import { NavLink} from 'react-router'
 export const Navbar = () => {
 
   return (
-    <div className='mx-auto max-w-auto w-[680px] flex border-1 border-slate-600 bg-stone-900 text-gray-200 rounded-full shadow-xl shadow-black/15'>
+    <div className='mx-auto max-w-auto md:w-[680px] w-[690px] flex border-1 border-slate-600 bg-stone-900 text-gray-200 rounded-full shadow-xl shadow-black/15 md:text-base text-sm'>
       <NavLink className='hover:shadow-[0_0_40px_5px_rgba(255,255,255,0.1)] transition duration-500 rounded-l-full py-4 px-8 hover:bg-gray-300 delay-150 hover:text-gray-900' to={'/'}>Home</NavLink>
       <NavLink className='hover:shadow-[0_0_40px_5px_rgba(255,255,255,0.1)] transition duration-500 p-2 px-4 py-4 hover:bg-gray-300 delay-100 hover:text-gray-900' to={'/Bayaan'}>Bayaan</NavLink>
       <NavLink className='hover:shadow-[0_0_40px_5px_rgba(255,255,255,0.1)] transition duration-500 p-2 px-8 py-4 hover:bg-gray-300 delay-100 hover:text-gray-900' to={'/N'}>न</NavLink>
       <NavLink className='hover:shadow-[0_0_40px_5px_rgba(255,255,255,0.1)] transition duration-500 p-2 px-4 py-4 hover:bg-gray-300 delay-100 hover:text-gray-900' to={'/Nayaab'}>Nayaab</NavLink>
-      <NavLink className='hover:shadow-[0_0_40px_5px_rgba(255,255,255,0.1)] transition duration-500 p-2 px-4 py-4 hover:bg-gray-300 delay-100 hover:text-gray-900' to={'/Lunchbreak'}>Lunch Break</NavLink>
+      <NavLink className='hover:shadow-[0_0_40px_5px_rgba(255,255,255,0.1)] transition duration-500  md:px-4 px-2 py-4 hover:bg-gray-300 delay-100 hover:text-gray-900 flex' to={'/Lunchbreak'}><p className='mr-2'>Lunch</p> <p>Break</p> </NavLink>
       <NavLink className='hover:shadow-[0_0_40px_5px_rgba(255,255,255,0.1)] transition duration-500 p-2 px-4 py-4 hover:bg-gray-300 delay-100 hover:text-gray-900' to={'/Kshama'}>Kshama</NavLink>
       <NavLink className='hover:shadow-[0_0_60px_5px_rgba(255,255,255,0.2)] transition rounded-r-full duration-500 p-2 px-6 py-4 hover:bg-gray-300 delay-100 hover:text-gray-900' to={'/Dl91'}>DL91</NavLink>
     </div>
--- src/pages/Lander.jsx
+++ src/pages/Lander.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export const Lander = () => {
   return (
-    <div className='h-screen w-full overflow-hidden absolute z-0 bg-[url("/main-bg.jpg")] bg-cover bg-center bg-no-repeat object-contain'>
+    <div className='h-screen w-full overflow-hidden z-0 md:bg-[url("/main-bg.jpg")] bg-[url(https://i.pinimg.com/736x/f3/f6/69/f3f669ac4445df8577681e6c597aaeb1.jpg)] bg-cover bg-center bg-no-repeat '>
       <div className='h-screen w-full absolute z-0 inset-0 bg-gradient-to-t from-black via-black/80 to-transparent'></div>
       <div className='relative z-10 h-full text-white flex flex-col justify-end' > 
-        <div className='russo-text my-12 mx-8'>
-          <p className='md:text-8xl text-5xl md-6 hover:bg-gradient-to-tl from-red-900 to-white hover:text-transparent bg-clip-text inline-block' id='ul'> Tera Bhai Seedhe Maut !!</p> 
-          <p className='text-2xl text-white my-3'> Stream high quality Seedhe maut songs without any ads</p>
+        <div className='russo-text mb-8 sm:mb-12 mx-8'>
+          <p className='text-3xl sm:text-4xl md:text-6xl lg:text-8xl md-6 hover:bg-gradient-to-tl from-red-900 to-white hover:text-transparent bg-clip-text inline-block' id='ul'> Tera Bhai Seedhe Maut !!</p> 
+          <p className='text-lg sm:text-xl md:text-2xl text-white my-3 leading-relaxed'> Stream high quality Seedhe maut songs without any ads</p>
           {/* <p className='font-mono font-bold text-2xl'>Made by a seedhe maut fan, for a seedhe maut fan</p> */}
         </div>
       </div>
